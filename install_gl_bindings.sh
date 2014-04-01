dfe838cde56a345f99516839b82a276cb3c4a1ae
iMurfyd <imdesjardin@gmail.com>
2014-04-01 16:28:48 -0400

    Changing script to get current directory correctly

--- install_gl_bindings.sh
+++ install_gl_bindings.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 #currently for mac os x with glfw installed in /opt/local (macports) ONLY!!!!
-export GOPATH=.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export GOPATH=$DIR
 go get github.com/go-gl/gl
 cd src/github.com/go-gl/gl
 CGO_CFLAGS="-I/opt/local/include/" CGO_LDFLAGS="-L/opt/local/lib/" go install
- go get github.com/go-gl/glfw3
- cd src/github.com/go-gl/glfw3
- CGO_CFLAGS="-I/opt/local/include/" CGO_LDFLAGS="-L/opt/local/lib/" go install
+go get github.com/go-gl/glfw3
+cd src/github.com/go-gl/glfw3
+CGO_CFLAGS="-I/opt/local/include/" CGO_LDFLAGS="-L/opt/local/lib/" go install
