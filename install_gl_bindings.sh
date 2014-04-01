ca7312b40e1005e3e620f1d5933e532dc5e5e85d
iMurfyd <imdesjardin@gmail.com>
2014-04-01 16:42:49 -0400

    Finalizing pkg install and whatnot

--- install_gl_bindings.sh
+++ install_gl_bindings.sh
@@ -2,9 +2,15 @@
 #currently for mac os x with glfw installed in /opt/local (macports) ONLY!!!!
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH=$DIR
+echo $GOPATH
+rm -r $GOPATH/src/github.com/go-gl
+echo "Getting gl"
 go get github.com/go-gl/gl
 cd src/github.com/go-gl/gl
+echo "Installing gl"
 CGO_CFLAGS="-I/opt/local/include/" CGO_LDFLAGS="-L/opt/local/lib/" go install
+echo "Getting glfw3"
 go get github.com/go-gl/glfw3
 cd src/github.com/go-gl/glfw3
+echo "Installing glfw3"
 CGO_CFLAGS="-I/opt/local/include/" CGO_LDFLAGS="-L/opt/local/lib/" go install
